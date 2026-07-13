98619d97220fb9657d52f947c2fd7e61a88e60b9
Your Name <vaishnavilingoji076@gmail.com>
2026-07-13 13:09:35 +0530

    aggrigation functions

---
+++ Aggrigation.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr=np.array([10,20,30,40,50])
+print(arr)
+
+print(np.sum(arr))
+print(arr.sum())
+
+print(np.mean(arr))
+print(np.max(arr))
+print(np.min(arr))
+
+#Product
+arr=np.array([2,3,4])
+print(np.prod(arr))
+
+
+#standard deviation
+arr=np.array([10,20,30,40,50])
+print(np.std(arr))
